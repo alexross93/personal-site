f9ba133c8532ec94c0ae7dbbbfca6e56709f1edc
Alex Ross <arossg17@gmail.com>
2019-04-18 16:41:43 -0400

    test without book in view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,14 +2,16 @@ const Book = require('../models/Book');
 
 exports.index = (req, res) => {
  
+  /* 
   Book.find({}, function(err, books) {
     var bookMap = {};
 
     books.forEach(function(book) {  
       bookMap[book._id] = book;
     });
-
-      res.render('index', {bookMap: bookMap});
   });
+  {bookMap: bookMap}
+  */
 
+ res.render('index');
 }
