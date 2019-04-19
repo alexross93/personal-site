fa5a6001e63a2cf7ddb4cf470237df577637e899
Alex Ross <arossg17@gmail.com>
2019-04-18 21:06:36 -0400

    changed test

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,23 +1,6 @@
 const Book = require('../models/Book');
 
 exports.index = (req, res) => {
- 
-  var error = 'start';
-  Book.find({}, function(err, books) {
-    if(err){error = 'no books';}
 
-    error = 'ok';
-  });
-
-  /* 
-  
-      var bookMap = {};
-
-    books.forEach(function(book) {  
-      bookMap[book._id] = book;
-    });
-  {bookMap: bookMap}
-  */
-
- res.render('index',{error: error});
+ res.render('index');
 }
